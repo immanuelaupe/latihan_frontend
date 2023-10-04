87042c9512ea9e729740f35b9e53304aba94a44f
unknown <s22210302@student.unklab.ac.id>
2023-10-04 19:45:51 +0800

    first commit front-end

---
+++ pertemuan-10/utils.js
@@ -0,0 +1,15 @@
+export let fullName = "John Doe"
+
+export let numbers = [1, 2, 3, 4]
+
+export let john = {
+    fullName: "John Doe",
+    age: 33,
+    address: "MANADO"
+};
+
+export const hello = () => {
+    console.log("Hello module");
+};
+
+export {fullName, numbers, const}
